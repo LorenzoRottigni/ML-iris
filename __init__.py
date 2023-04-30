bed861dcc22b6e488ade463b538e940057c1de13
LorenzoRottigni <lorenzo@rottigni.net>
2023-04-30 21:35:06 +0200

    SCAFFOLD: ML2

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_iris
+
+# Load the iris flower dataset
+dataset = load_iris()
+
+print(dataset['DESC'])
+X = dataset['data']
+y = dataset['target']
+
+# 1st Flower morphologic features
+#flower_record_1_input = X[0]
+# 1st Flower species
+#flower_record_1_output = y[0]
+
+# TODO: go on https://www.youtube.com/watch?v=lMJqXncEn78
+
